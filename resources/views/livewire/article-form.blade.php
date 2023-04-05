8d1285d738c83f17a2bf9fcf80a45d363ff33cdd
kevincisnero01 <kevingabriel18@gmail.com>
2023-04-05 10:44:56 -0400

    16 - Styling with Tailwind

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -1,29 +1,50 @@
 <div>
-<div class="container mx-auto py-4 px-8">
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('New Article') }}
+    </h2>
+</x-slot>
+<div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <x-form-section submit="save">
 
-    <h1 class="text-3xl font-bold underline my-2">Formulario</h1>
-    
-    <a class="px-4 py-1 bg-gray-200 rounded hover:bg-gray-300" href="{{ route('articles.index') }}" >Listado de  Articulo</a>
+        <x-slot name="title">
+            {{ __('New Article') }}
+        </x-slot>
 
-    <form wire:submit.prevent="save">
-        <label class="block my-4">
-            <input wire:model="article.title" type="text" placeholder="Titulo..." class="rounded">
-            @error('article.title') <div>{{ $message }}</div> @enderror
-        </label>
+        <x-slot name="description">
+            {{ __('Some description') }}
+        </x-slot>
 
-        <label class="block my-4">
-            <input wire:model="article.slug" type="text" placeholder="URL Amigable..." class="rounded">
-            @error('article.slug') <div>{{ $message }}</div> @enderror
-        </label>
+        <x-slot name="form">
 
-        <label class="block my-4">
-            <textarea wire:model="article.content" placeholder="Contenido..." class="rounded"></textarea>
-            @error('article.content') <div>{{ $message }}</div> @enderror
-        </label>
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="title" :value="__('Titlte')" />
+                <x-input wire:model="article.title" id="title" type="text" class="mt-1 block w-full"/>
+                <x-input-error for="article.title" class="mt-1"/>
+            </div>
+            
 
-        <button type="submit" class="px-4 py-1 bg-gray-200 rounded hover:bg-gray-300" >
-            Enviar
-        </button>
-    </form>
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="slug" :value="__('Slug')" />
+                <x-input wire:model="article.slug" id="slug" type="text" class="mt-1 block w-full"/>
+                <x-input-error for="article.slug" class="mt-1"/>
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="content" :value="__('Content')" />
+                <x-input-textarea wire:model="article.content" id="content" class="mt-1 block w-full"/>
+                <x-input-error for="article.content" class="mt-1"/>
+            </div>
+
+            <x-slot name="actions">
+                <x-button>
+                    {{ __('Save') }}
+                </x-button>
+            </x-slot>
+
+        </x-slot>
+    </x-form-section>
+    </div>
 </div>
 </div>
