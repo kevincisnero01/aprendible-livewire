07c891aba24d299802f8c37593fcbde70e9dcbe9
kevincisnero01 <kevingabriel18@gmail.com>
2023-04-14 16:07:18 -0400

    21 - Styling Input Image

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -18,9 +18,19 @@
 
         <x-slot name="form">
 
-            <div class="col-span-6 sm:col-span-4">
-                <x-label for="image" :value="__('Image')" />
-                <x-input wire:model="image" id="image" type="file" class="mt-1 block w-full"/>
+            <div class="col-span-6 sm:col-span-4 relative">
+            @if($image)
+                <x-danger-button wire:click="$set('image')" class="absolute bottom-2 right-2" >{{ __('Change Image') }}</x-danger-button>
+                <image src="{{ $image?->temporaryUrl() }}" class="border-2 rounded" alt="imagen">
+            @elseif($article->image)
+                <x-label for="image" :value="__('Change Image')" class="absolute bottom-2 right-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"/>
+                <img src="{{ Storage::disk('public')->url($article->image) }}" class="border-2 rounded" alt="imagen"> 
+            @else
+                <div class="h-32 bg-gray-50 border-2 border-dashed rounded flex items-center justify-center">
+                    <x-label for="image" :value="__('Select Image')" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"/>
+                </div>
+            @endif
+                <x-input wire:model="image" id="image" type="file" class="mt-1 block w-full hidden"/>
                 <x-input-error for="image" class="mt-1"/>
             </div>
 
