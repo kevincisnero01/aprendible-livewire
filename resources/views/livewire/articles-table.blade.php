b68d3e56dcaac3cd8aa73d4bc20122151c69af09
kevincisnero01 <kevingabriel18@gmail.com>
2023-04-22 10:48:52 -0400

    34 - Upgrade Livewire
    -fix test

--- resources/views/livewire/articles-table.blade.php
+++ resources/views/livewire/articles-table.blade.php
@@ -1,6 +1,5 @@
 <div class="py-10">
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-
     <div class="flex justify-between">
         <x-input 
             wire:model="search" 
@@ -16,66 +15,45 @@
             {{ __('New Article') }}
         </x-button-link>
     </div>
-
-
     
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="p-4 col-span-2">
-                    {{ __('Title') }}
-                </th>
-                <th scope="col" class="p-4">
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{ __('Created At') }}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($articles as $article)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center pl-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                    <img class="w-10 h-10 rounded-full" src="{{ $article->image_url }}" alt="Imagen Article">
-                </th>
-                <td class="pr-6 py-4">
-                    {{ $article->title }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ $article->created_at->diffForHumans() }}
-                </td>
-                <td class="px-6 py-4">
-                    <a href="{{ route('articles.edit', $article->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"> 
-
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                        </svg>
-
-                    </a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
-
-
-
-{{--     
-    <ul class="list-disc list-inside">
-            <li>
-                <a href="{{ route('articles.show', $article) }}">
-                    {{ $article->title }}
-                </a>
-                <a href="{{ route('articles.edit', $article) }}" class="px-2 underline text-blue-600 active:text-blue-800">
-                    Editar
-                </a>
-            </li>
-        
-    </ul> --}}
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+        <table class="w-full text-sm text-left text-gray-500 ">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                <tr>
+                    <th scope="col" class="p-4" >
+                        {{ __('Title') }}
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        {{ __('Created At') }}
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-center">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($articles as $article)
+                <tr class="bg-white border-b hover:bg-gray-50 ">
+                    <th class="flex items-center gap-2 px-6 py-4 text-gray-900">
+                        <img class="w-10 h-10 rounded-full border border-gray-300" src="{{ $article->image_url }}" alt="Imagen Article">
+                        <a href="{{ route('articles.show', $article) }}"> 
+                            {{ $article->title }}
+                        </a>
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $article->created_at->diffForHumans() }}
+                    </td>
+                    <td class="px-6 py-4 text-center">
+                        <a href="{{ route('articles.edit', $article->id) }}" class="inline-block font-medium text-blue-600"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 </div>
--- tests/Feature/Livewire/ArticlesTableTest.php
+++ tests/Feature/Livewire/ArticlesTableTest.php
@@ -2,17 +2,21 @@
 
 namespace Tests\Feature\Livewire;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ArticlesTest extends TestCase
+class ArticlesTableTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_articles_component_renders_properly(): void
     {
-        $this->get(route('articles.index'))
-            ->assertSeeLivewire('articles');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->get(route('articles.index'))
+            ->assertSeeLivewire('articles-table');
     }
 }
